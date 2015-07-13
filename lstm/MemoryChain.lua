5f7ee679b69006dee7e3731b5a2ec8e4f951a541
Kevin Bullaughey <kbullaughey@gmail.com>
2015-07-13 16:17:51 -0400

    get it working in batch mode, accounting for variable length sequences

--- lstm/MemoryChain.lua
+++ lstm/MemoryChain.lua
@@ -47,21 +47,17 @@ function MemoryChain:parameters()
   return self.lstm_params, self.lstm_grad_params
 end
 
+-- Receives a table containing two Tensors: input and a vector of lengths, as not all
+-- sequences will span the full length dimension of the tensor.
 -- If input is 3D then the first dimension is batch, otherwise the first dim
 -- is the sequence. Last dimension is features.
-function MemoryChain:updateOutput(input)
-  if input:dim() == 3 then
-    self.batchSize = input:size(1)
-    self.len = input:size(2)
-  elseif input:dim() == 2 then
-    self.batchSize = 1
-    self.len = input:size(1)
-  else
-    error("expecting either 2D or 3D input")
+function MemoryChain:updateOutput(tuple)
+  local input, lengths = unpack(tuple)
+  if input:dim() ~= 3 then
+    error("expecting a 3D input")
   end
-  -- Standarize our view of the input so we can always count on the first dimension
-  -- being batch size, the second length, and third the number of input featuers.
-  local inputView = input:view(self.batchSize, self.len, self.inputSize)
+  self.batchSize = input:size(1)
+  self.len = input:size(2)
   -- The first memory cell will receive zeros.
   local h = lstm.Tensor()(self.batchSize, self.hiddenSize):zero()
   local c = lstm.Tensor()(self.batchSize, self.hiddenSize):zero()
@@ -71,24 +67,62 @@ function MemoryChain:updateOutput(input)
   -- Iterate over memory cells feeding each successive tuple (h,c) into the next
   -- LSTM memory cell.
   for i=1,self.len do
-    local x = inputView:select(2, i)
+    local x = input:select(2, i)
     self.lstms[i]:forward({x, h, c})
     h, c = unpack(self.lstms[i].output)
     self.hidden_states[i] = h
     self.memories[i] = c
   end
-  self.output = self.lstms[self.len].output
+  -- Copy the terminal output for each batch member into the output tensor.
+  self.output = lstm.Tensor()(self.batchSize, self.hiddenSize)
+  for b=1, self.batchSize do
+    h = self.lstms[lengths[b]].output[1]
+    self.output[b]:copy(h[b])
+  end
   return self.output
 end
 
-function MemoryChain:updateGradInput(input, gradOutput)
+-- upstreamGradOutput will be a BxH matrix where B is batch size and H is hidden
+-- state size. Each row will correspond to the gradient of the object function
+-- wrt outputs from the LSTM memory cell corresponding to the sequence terminus.
+-- However, this isn't necessarily the last memory cell in the `lstms` array
+-- because sequences are different lengths.
+function MemoryChain:updateGradInput(tuple, upstreamGradOutput)
+  local input, lengths = unpack(tuple)
   local h,c
-  local inputView = input:view(self.batchSize, self.len, self.inputSize)
-  self.gradInput = lstm.Tensor()(self.batchSize, self.len, self.inputSize)
-  for i=len,1,-1 do
-    local x = inputView:select(2,i)
+  if input:dim() ~= 3 then
+    error("MemoryChain:updageGradInput is expecting a 3D input tensor")
+  end
+  self.gradInput = lstm.Tensor()(self.batchSize, self.len, self.inputSize):zero()
+  -- Because each batch member has a sequence of a different length less than
+  -- or equal to self.len, we need to have some way to propagate errors starting
+  -- at the correct level. 
+  --
+  -- I build a binary matrices of size BxL. This matrix will be used to
+  -- determine from where error terms are propagating back from. The matrix,
+  -- terminal, has a one at the terminal column in the sequence. 
+  local terminal = lstm.Tensor()(self.batchSize, self.len):zero()
+  for b=1,self.batchSize do
+    local l = lengths[b]
+    terminal[b][l] = 1
+  end
+  -- Since we only use the hidden state for prediction, the gradient wrt to the
+  -- memory is zero. And since beyond the end of the sequence the extra memory
+  -- cells can't affect the objective function, the gradient wrt to the extra
+  -- hidden states is also zero.
+  local gradOutput = {
+    lstm.Tensor()(self.batchSize, self.hiddenSize):zero(),
+    lstm.Tensor()(self.batchSize, self.hiddenSize):zero()
+  }
+  for i=self.len,1,-1 do
+    -- Replicate our mask of which batch members receive errors from the
+    -- upstream gradient at this time step. (replicated hiddenSize times)
+    local terminalColumn = terminal:select(2, i):contiguous():view(-1,1)
+    local upstream = torch.mm(terminalColumn, torch.ones(1,self.hiddenSize))
+    local x = input:select(2,i)
     h = self.hidden_states[i-1]
     c = self.memories[i-1]
+    gradOutput[1]:add(upstream:cmul(upstreamGradOutput))
     self.lstms[i]:backward({x,h,c}, gradOutput)
     gradOutput = self.lstms[i].gradInput
     -- Only h and c propagate back to the next cell, the gradient wrt x gets stored
--- lstm/init.lua
+++ lstm/init.lua
@@ -5,6 +5,7 @@ lstm = {}
 lstm.MemoryCell = require 'lstm.MemoryCell'
 require 'lstm.MemoryChain'
 require 'lstm.SelectTerminal'
+require 'lstm.BasicNet'
 
 lstm.ping = function()
   return "pong"
@@ -17,12 +18,26 @@ lstm.cuda = function()
   use_cuda = true
 end
 
+-- Synchronize if usign cuda or just a no-op
+lstm.sync = function()
+  if use_cuda then
+    cutorch.synchronize()
+  end
+end
+
 -- Choose a tensor constructor
 lstm.Tensor = function()
   if use_cuda then
     return torch.CudaTensor
   end
   return torch.Tensor
+end
+
+lstm.localize = function(thing)
+  if use_cuda then
+    return thing:cuda()
+  end
+  return thing
 end
 
 return lstm
