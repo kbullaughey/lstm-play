d711d9a4c93342d60cd625c98a5438b5a7c1df45
Kevin Bullaughey <kbullaughey@gmail.com>
2015-10-07 09:26:15 -0400

    remove reversed

--- toys/toy/variable_width_2-4.sh
+++ toys/toy/variable_width_2-4.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-th variable_width_2-4-reversed.lua -n 200000 -sd 0.1 -seed 1 \
-  -outfile 'variable_width_2-4-reversed.t7'
+th variable_width_2-4.lua -n 200000 -sd 0.1 -seed 1 \
+  -outfile 'variable_width_2-4.t7'
