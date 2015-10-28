ab2ee557087cbfdc03a7ce77bc52dc897051f3ca
Kevin Bullaughey <kbullaughey@gmail.com>
2015-10-28 16:41:22 -0400

    pasting maps together for efficiency

--- lstm/MemoryCell.lua
+++ lstm/MemoryCell.lua
@@ -25,25 +25,31 @@ local MemoryCell = function(prev_h, prev_c, x, inputSize, hiddenSize)
   -- Equation numbers refer to Graves (2014) Generating sequences with recurrent
   -- neural networks.
 
+  -- Since x and prev_h are each fed through four linear maps, we can do the four maps
+  -- together and later split them out. Also, since x mapped to hidden-space and
+  -- prev_h mapped to hidden-space are always added, we can do that up-front too.
+  local xhm = nn.CAddTable()({
+    Linear(inputSize,hiddenSize*4)(x),
+    Linear(hiddenSize,hiddenSize*4)(prev_h)
+  })
+
+  -- Split the four xh mappings appart.
+  local xhm_split = nn.SplitTable(2)(nn.Reshape(4,hiddenSize)(xhm))
+
   -- Input gate. Equation (7)
   local i_gate = nn.Sigmoid()(nn.CAddTable()({
-    Linear(inputSize,hiddenSize)(x),
-    Linear(hiddenSize,hiddenSize)(prev_h),
+    nn.SelectTable(1)(xhm_split),
     Linear(hiddenSize,hiddenSize)(prev_c)
   }))
 
   -- Forget gate. Equation (8)
   local f_gate = nn.Sigmoid()(nn.CAddTable()({
-    Linear(inputSize,hiddenSize)(x),
-    Linear(hiddenSize,hiddenSize)(prev_h),
+    nn.SelectTable(2)(xhm_split),
     Linear(hiddenSize,hiddenSize)(prev_c)
   }))
 
   -- New contribution to c. Right term in equation (9)
-  local learning = nn.Tanh()(nn.CAddTable()({
-    Linear(inputSize,hiddenSize)(x),
-    Linear(hiddenSize,hiddenSize)(prev_h)
-  }))
+  local learning = nn.Tanh()(nn.SelectTable(3)(xhm_split))
 
   -- Memory cell. Equation (9)
   local c = nn.CAddTable()({
@@ -53,8 +59,7 @@ local MemoryCell = function(prev_h, prev_c, x, inputSize, hiddenSize)
 
   -- Output gate. Equation (10)
   local o_gate = nn.Sigmoid()(nn.CAddTable()({
-    Linear(inputSize,hiddenSize)(x),
-    Linear(hiddenSize,hiddenSize)(prev_h),
+    nn.SelectTable(4)(xhm_split),
     Linear(hiddenSize,hiddenSize)(c)
   }))
 
