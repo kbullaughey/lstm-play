2c963f3cc3259b9c241675fb08f60834ac90f972
Kevin Bullaughey <kbullaughey@gmail.com>
2016-02-26 21:57:38 -0500

    add batch mode for InvertIndex

--- lstm/InvertIndex.lua
+++ lstm/InvertIndex.lua
@@ -2,7 +2,8 @@ local InvertIndex, parent = torch.class('lstm.InvertIndex', 'nn.Module')
 
 -- InvertIndex takes as input a 1D LongTensor of length <= size containing index
 -- positions in the range of 1 <= i <= size. It outputs a vector of length
--- size with 1 copied into the indexed cells.
+-- size with 1 copied into the indexed cells. If used in batch mode, it can take
+-- a table of 1D tensors.
 
 -- size takes the length of the resulting indicator vector
 function InvertIndex:__init(size)
@@ -11,9 +12,15 @@ function InvertIndex:__init(size)
 end
 
 function InvertIndex:updateOutput(indexes)
-  local t = input[1]
-  local index = input[2]
-  self.output:resize(self.size):zero()
-  self.output:indexFill(1, indexes, 1)
+  if type(indexes) == "table" then
+    local batchSize = #indexes
+    self.output:resize(batchSize, self.size):zero()
+    for i=1,batchSize do
+      self.output[i]:indexFill(1, indexes[i], 1)
+    end
+  else
+    self.output:resize(self.size):zero()
+    self.output:indexFill(1, indexes, 1)
+  end
   return self.output
 end
--- toys/invert_index/invert_index_test.lua
+++ toys/invert_index/invert_index_test.lua
@@ -18,3 +18,11 @@ node:backward(indexes, torch.Tensor{1,2,3,4})
 if node.gradInput:dim() ~= 0 then
   error("expecting dimensionless gradient")
 end
+
+-- It can work in batch mode.
+indexes = {torch.LongTensor{1,4,2}, torch.LongTensor{2,3}}
+res = node:forward(indexes)
+
+if res:eq(torch.Tensor{{1,1,0,1},{0,1,1,0}}):sum() ~= 8 then
+  error("inverting didn't get expected response")
+end
