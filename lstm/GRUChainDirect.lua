0d49471a5801dca3bd25514bbf5815eabff0ee54
Kevin Bullaughey <kbullaughey@gmail.com>
2017-03-10 09:54:38 -0500

    since GRUChainDirect only supports one layer, remove unnecessary iteration over layers

--- lstm/GRUChainDirect.lua
+++ lstm/GRUChainDirect.lua
@@ -30,34 +30,28 @@ function Class:updateOutput(tuple)
   local localLengths = lengths:typeAs(torch.Tensor())
 
   -- Storage for output
-  local topLayer = self.numLayers
-  local topLayerSize = self.hiddenSizes[topLayer]
-  self.output:resize(batchSize, longestExample, topLayerSize):zero()
+  assert(self.numLayers == 1, "Only one layer supported")
+  local layerSize = self.hiddenSizes[1]
+  self.output:resize(batchSize, longestExample, layerSize):zero()
 
-  for l=1, self.numLayers do
-    local h = self:initialState(l, batchSize)
+  local h = self:initialState(1, batchSize)
 
-    -- Iterate over cells feeding each successive h into the next
-    -- GRU cell.
-    for t=1,longestExample do
-      local x
-      if l == 1 then
-        x = input:select(2, t)
-      else
-        x = self.grus[l-1][t].output
-      end
-      assert(self.grus[l], "Missing gru at layer " .. l)
-      assert(self.grus[l][t], "Missing gru at layer " .. l .. " timestep " .. t)
-      h = self.grus[l][t]:forward({h, x})
-    end
+  -- Iterate over cells feeding each successive h into the next
+  -- GRU cell.
+  for t=1,longestExample do
+    local x
+    x = input:select(2, t)
+    assert(self.grus[1], "Missing grus")
+    assert(self.grus[1][t], "Missing gru at timestep " .. t)
+    h = self.grus[1][t]:forward({h, x})
   end
 
-  -- Copy the output of the top layer for each batch member into the output tensor.
+  -- Copy the output for each batch member into the output tensor.
   for b=1, batchSize do
     local batchMemberLength = localLengths[b]
     if batchMemberLength > 0 then
       for t=1,batchMemberLength do
-        local unit = self.grus[topLayer][t]
+        local unit = self.grus[1][t]
         if unit == nil then
           error("No unit at timestep " .. t .. " for batch member " .. b)
         end
@@ -91,71 +85,49 @@ function Class:updateGradInput(tuple, upstreamGradOutput)
   end
 
   -- We actually only have one layer, but this code comes from GRUChain
-  local topLayer = self.numLayers
-  for l=topLayer,1,-1 do
-    local thisHiddenSize = self.hiddenSizes[l]
-    -- Resize scratch memory so it's the right size for this layer.
-    self.h:resize(batchSize, thisHiddenSize)
-    self.gradOutputScratch:resize(batchSize, thisHiddenSize)
+  assert(self.numLayers == 1, "Only one layer supported")
+  local hiddenSize = self.hiddenSizes[1]
+  -- Resize scratch memory so it's the right size for this layer.
+  self.h:resize(batchSize, hiddenSize)
+  self.gradOutputScratch:resize(batchSize, hiddenSize)
 
-    for t=len,1,-1 do
-      local gradOutput = self.gradOutputScratch:zero()
-      if t == len then
-        if l == topLayer then
-          -- This is the top, right corner. The only gradient we get is from
-          -- upstreamGradOutput. The gradOutput for this cell is only non-zero
-          -- where there are batch members that teriminate here.
-          for b=1,batchSize do
-            if localLengths[b] == t then
-              gradOutput[b]:add(upstreamGradOutput[b][t])
-            end
-          end
-        else
-          -- right edge, but not top layer
-          local gruAbove = self.grus[l+1][t]
-	        -- Gradient for h
-          gradOutput = gruAbove.gradInput[1]
-        end
-      else
-        -- Not the right-most, so we need to account for messages incoming on the
-        -- right.
-        local gruRight = self.grus[l][t+1]
-        gradOutput = gruRight.gradInput[1]
-        if l == topLayer then
-          -- Only copy messages from above if the batch member is at least this long.
-          for b=1,batchSize do
-            if localLengths[b] >= t then
-              gradOutput[b]:add(upstreamGradOutput[b][t])
-            end
-          end
-        else
-          -- Not top layer, so just take grad from above.
-          local gruAbove = self.grus[l+1][t]
-          -- The h output of this gru is the x input of the one above it.
-          gradOutput:add(gruAbove.gradInput[2])
+  for t=len,1,-1 do
+    local gradOutput = self.gradOutputScratch:zero()
+    if t == len then
+      -- This is the right side. The only gradient we get is from
+      -- upstreamGradOutput. The gradOutput for this cell is only non-zero
+      -- where there are batch members that teriminate here.
+      for b=1,batchSize do
+        if localLengths[b] == t then
+          gradOutput[b]:add(upstreamGradOutput[b][t])
         end
       end
+    else
+      -- Not the right-most, so we need to account for messages incoming on the
+      -- right.
+      local gruRight = self.grus[1][t+1]
+      gradOutput = gruRight.gradInput[1]
+      -- Only copy messages from above if the batch member is at least this long.
+      for b=1,batchSize do
+        if localLengths[b] >= t then
+          gradOutput[b]:add(upstreamGradOutput[b][t])
+        end
+      end
+    end
 
-      -- Backward propagate this cell
-      local x, h
-      if l == 1 then
-        x = input:select(2,t)
-      else
-        x = self.grus[l-1][t].output
-      end
-      if t == 1 then
-        h = self.h:zero()
-      else
-        h = self.grus[l][t-1].output
-      end
-      self.grus[l][t]:backward({h, x}, gradOutput)
-      -- If we're the bottom layer, we need to update gradInput
-      if l == 1 then
-        self.allGradInput[2]:select(2, t):copy(self.grus[1][t].gradInput[2])
-        if t == 1 then
-          self.allGradInput[1]:copy(self.grus[1][1].gradInput[1])
-        end
-      end
+    -- Backward propagate this cell
+    local x, h
+    x = input:select(2,t)
+    if t == 1 then
+      h = self.h:zero()
+    else
+      h = self.grus[1][t-1].output
+    end
+    self.grus[1][t]:backward({h, x}, gradOutput)
+    -- We need to update gradInput
+    self.allGradInput[2]:select(2, t):copy(self.grus[1][t].gradInput[2])
+    if t == 1 then
+      self.allGradInput[1]:copy(self.grus[1][1].gradInput[1])
     end
   end
   return self.gradInput
